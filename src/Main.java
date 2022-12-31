513a0ca6e1aa8c1ed588b098b39cafe8b2f3627e
PioneerX7 <vita-kopy@yandex.ru>
2022-12-31 13:47:07 +0300

    all tasks

--- src/Main.java
+++ src/Main.java
@@ -5,23 +5,22 @@ public class Main {
         task3();
     }
 
-    public static int checkingYear(int yearCheck) {
+    public static void checkingYear(int yearCheck) {
         if (yearCheck % 4 == 0 && yearCheck % 100 != 0 || yearCheck % 400 == 0) {
             System.out.println(yearCheck + " год - високосный год");
         } else {
             System.out.println(yearCheck + " год - невисокосный год");
         }
-        return yearCheck;
     }
 
     public static void task1() {
         System.out.println("Задача 1");
         int year = 2022;
-        int result = checkingYear(year);
+        checkingYear(year);
 
     }
 
-    public static int applicationSelection(int clientOS, int currentYear) {
+    public static void applicationSelection(int clientOS, int currentYear) {
 
         if (clientOS == 0) {
             if (currentYear >= 2015) {
@@ -38,18 +37,17 @@ public class Main {
         } else {
             System.out.println("Введите корректные данные для продолжения");
         }
-        return clientOS;
     }
 
     public static void task2() {
         System.out.println("Задача 2");
         int clientOS = 0;
         int currentYear = 2022;
-        int result = applicationSelection(clientOS, currentYear);
+        applicationSelection(clientOS, currentYear);
 
     }
 
-    public static int deliveryTime(int deliveryDistance) {
+    public static void deliveryTime(int deliveryDistance) {
         int deliveryDay = 1;
         if (deliveryDistance < 20) {
             System.out.println("Потребуется дней: " + deliveryDay);
@@ -62,12 +60,11 @@ public class Main {
         } else {
             System.out.println("Доставка не осуществляется.");
         }
-        return deliveryDistance;
     }
 
     public static void task3() {
         System.out.println("Задача 3");
         int deliveryDistance = 95;
-        int result = deliveryTime(deliveryDistance);
+        deliveryTime(deliveryDistance);
     }
 }
